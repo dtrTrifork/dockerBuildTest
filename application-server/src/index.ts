03a94887760341b61c4007f047c06eb95a75ba79
dtr <dtr@trifork.com>
2021-09-16 13:39:31 +0200

    added more code to index

--- application-server/src/index.ts
+++ application-server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello Docker")
+import axios from "axios"
+
+const instance = axios.create({
+    baseURL: "https://pokeapi.co/api/v2/"
+})
+
+const checkPokemon = async (pokemonName: string) => {
+    const pokemon = await instance.get(`pokemon/${pokemonName}`)
+    return pokemon.data
+}
+console.log("Hello Docker", checkPokemon("ditto"))
